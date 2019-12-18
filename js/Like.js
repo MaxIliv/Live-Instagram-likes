5246a6170ba1cc23c0847d84cee02c6cbfc7d2f6
Max Iliv <max.r.iliv@gmail.com>
2019-12-18 21:08:47 +0200

    Likes alive

--- js/Like.js
+++ js/Like.js
@@ -1,8 +1,62 @@
 class Like {
-  constructor() {
-    
+  constructor(props) {
+    const { x = 0, y = 0, color, remove } = props;
+
+    this.x = x;
+    this.y = y;
+    this.r = 0.3;
+    this.velX = Math.random() > 0.5 ? -(Math.random()) : Math.random();
+    this.velY = Math.ceil(Math.random() * 6) + 1;
+    this.accX = 0.02;
+    this.accY = 0.01;
+    this.color = color;
+    this.remove = remove;
   }
 
+  update() {
+    if (this.velX > 1 || this.velX < -1) {
+      this.accX = -this.accX;
+    }
+
+    // if (this.velY > 3 || this.velY < -3) {
+    //   this.accY = -this.accY;
+    // }
+    // this.velY += this.accY;
+    this.velX += this.accX;
+    
+
+    this.x += this.velX;
+    this.y -= this.velY;
+  }
+
+  draw() {
+    this.update();
+
+    ctx.save();
+    ctx.beginPath();
+    // draw heart
+    this.drawHeart();
+    //
+    ctx.fillStyle = this.color;
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+  }
+
+  checkBound() {
+    if (this.y < 0) {
+      return true;
+    }
+  }
+
+  drawHeart() {
+    ctx.moveTo(this.x, this.y);
+    for (let a = 0; a < Math.PI * 2; a+=0.01) {
+      const x = this.r * 16 * Math.pow(Math.sin(a), 3);
+      const y = -1 * this.r * (13 * Math.cos(a) - 5 * Math.cos(2*a) - 2 * Math.cos(3*a) - Math.cos(4*a));
+      ctx.lineTo(this.x + x, this.y + y);
+    }
+  }
   get name() {
     return 'I am a Like';
   }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,98 @@
 const likes = [];
+const colors = ['pink', 'lightblue', 'lightcoral', 'olive', 'aliceblue', ];
+
+const width = canvas.width;
+const height = canvas.width;
+
+const center = {
+  x: canvas.width / 2,
+  y: canvas.height / 2
+};
+
+setup();
+
+function getProps() {
+  return {
+    ...bottomPos(),
+    ...getColor(),
+    remove: remove
+  }
+}
+
+function remove(item) {
+  for (let i = 0, len = likes.length; i < len; i++) {
+      if (likes[i] === item) {
+        likes.splice(i, 1);
+        break;
+      }
+  }
+}
+
+function randomPos() {
+  return {
+    x: width * Math.random(),
+    y: height * Math.random()
+  }
+}
+
+function bottomPos() {
+  return {
+    x: width / 2,
+    y: height
+  }
+}
+
+function setup() {
+  bindEvents();
+
+  draw();
+}
+ 
+function draw() {
+  background();
+  drawItems();
+  requestAnimationFrame(draw);
+}
+
+function drawItems() {
+  for (let i = likes.length; i--;) {
+    if (likes[i].checkBound()) {
+      this.remove(likes[i]);
+      continue;
+    }
+    likes[i].draw();
+  }
+}
+
+function background() {
+  ctx.clearRect(0, 0, width, height);
+}
+
+function like() {
+  likes.push(new Like(getProps()));
+}
+
+function likeMore() {
+  for (let i = 0; i < 10; i++) {
+    like();
+  }
+}
+
+function bindEvents() {
+  canvas.addEventListener('click', () => {
+    likes.push(new Like(getProps()));
+  });
+
+  const likeBtn = document.getElementById('like');
+  const likesBtn = document.getElementById('likes');
 
 
-for (let i = 0; i < 100; i++) {
-    likes.push(new Like());
+  likeBtn.addEventListener('click', like);
+  likesBtn.addEventListener('click', likeMore);
+}
+
+function getColor() {
+  return {
+    color: colors[Math.floor(Math.random() * colors.length)]
+  };
 }
